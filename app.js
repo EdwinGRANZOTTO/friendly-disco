eaecadca7cab37f8506c73d51067e1c229aa6b7d
EdwinGRANZOTTO <ed.granzotto@gmail.com>
2020-04-29 16:23:23 +0200

    OK routes

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs'); 
+app.set('views', 'views'); 
+app.use(express.static(__dirname +'/assets'));
+
 app.get('/', function (req, res) {
+  res.render('index.ejs')
+})
+
+app.get('/gallery', function (req, res) {
+  res.render('gallery.ejs')
+})
+
+app.get('/generic', function (req, res) {
   res.render('generic.ejs')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-app.set('view engine', 'ejs'); 
-app.set('views', 'views'); 
-app.use(express.static('assets'));
